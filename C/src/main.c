2fd0099f257df6b317ff2b75525a1091c104b891
tbebekis <teo.bebekis@gmail.com>
2024-10-21 14:53:12 +0300

    .

--- C/src/main.c
+++ C/src/main.c
@@ -1,13 +1,14 @@
 #include "funcs.h"
-#include <stdio.h>
- 
+#include <stdio.h> 
 
 int main() {
    GetInteger();
- 
+
+#ifdef _WIN32 
     printf("Hit any key + Enter to close. ");
     int c;
-    scanf("%d", &c);
+    scanf("%d", &c); 
+#endif 
  
     return 0;
 }
